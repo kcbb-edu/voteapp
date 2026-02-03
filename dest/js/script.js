72f97fb641a0494527e5c4aedbc12878d7a63898
Lui Wa Hong <luiwahong.it@gmail.com>
2026-02-03 12:24:57 +0800

    Update client script with full session code login flow

--- dest/js/script.js
+++ dest/js/script.js
@@ -3,12 +3,82 @@ let scoredUsers = [],
     isScored = false,
     randomId;
 
-// Load saved code
+function preventZoom(e) {
+  var t2 = e.timeStamp;
+  var t1 = e.currentTarget.dataset.lastTouch || t2;
+  var dt = t2 - t1;
+  var fingers = e.touches.length;
+  e.currentTarget.dataset.lastTouch = t2;
+
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault();
+  e.target.click();
+}
+
+function rand() {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+function showConfirmMsg(msg, shakeVal) {
+    if (shakeVal >= 0 && $.fn.effect) {
+        $('.confirmWrapper').effect('shake', { times: 2, distance: 5 }, 500);
+    }
+    $('.confirmMsg').text(msg).fadeIn();
+}
+
+// Logic to switch UI
+function showLogin() {
+    $('.loginWrapper').css('display', 'flex'); // Flex for centering
+    $('.scoreBtnWrapper').hide();
+    $('.confirmMsg').text('');
+}
+
+function showVoting(code) {
+    $('.loginWrapper').hide();
+    $('.scoreBtnWrapper').show();
+    $('.roomCodeDisplay').text(code);
+}
+
+function joinSession(code, silent = false) {
+    if(!code) {
+        if(!silent) {
+            showConfirmMsg('請輸入房號', 0);
+            setTimeout(() => showConfirmMsg(' '), 2000);
+        }
+        return;
+    }
+    
+    socket.emit('checkCode', code, (isValid) => {
+         if (isValid) {
+             localStorage.setItem('sessionCode', code);
+             showVoting(code);
+             if(!silent) $('.confirmMsg').text(" "); 
+         } else {
+             localStorage.removeItem('sessionCode');
+             showLogin();
+             if(!silent) {
+                 showConfirmMsg('房號錯誤 (Wrong Code)', 0);
+                 setTimeout(() => showConfirmMsg(' '), 2000);
+             }
+         }
+    });
+}
+
 $(document).ready(() => {
+    // Check if we have a saved code
     const savedCode = localStorage.getItem('sessionCode');
     if (savedCode) {
         $('.sessionCodeInput').val(savedCode);
+        joinSession(savedCode, true); // true = silent check
+    } else {
+        showLogin();
     }
+});
+
+$('.btnJoin').on('click', () => {
+    const code = $('.sessionCodeInput').val();
+    joinSession(code);
 });
 
 $('.scoreBtn').on('click', (event) => {
@@ -17,107 +87,63 @@ $('.scoreBtn').on('click', (event) => {
         randomId = rand();
         localStorage.setItem('userId', randomId);
     }
+    
+    // Safety check: Ensure we have a code
+    const code = localStorage.getItem('sessionCode') || $('.sessionCodeInput').val();
 
-    showConfirmMsg('發送中...');
-    console.log('OnClick: ' + $(event.target).data('score'));
-
-    const code = $('.sessionCodeInput').val();
     if (!code) {
-        showConfirmMsg('請輸入房號', 0);
-        setTimeout(() => showConfirmMsg(' '), 2000);
+        showLogin();
         return;
     }
 
+    showConfirmMsg(' 發送中...');
+
     socket.emit('authUser', { 'withData': true, 'socreClicked': $(event.target).data('score')}, (confirmation) =>{
-        console.log(confirmation);
         
-        if (!isScored) {        
-            socket.emit('score', { userId: randomId, score: $(event.target).data('score'), code: code }, (confirmation) => {
-                showConfirmMsg(confirmation, 0);
-                if (confirmation.includes('Error')) {
-                   // Don't disable if error
-                } else {
-                   // Success
-                   localStorage.setItem('sessionCode', code);
-                   if (!confirmation) {
-                       localStorage.removeItem('userId');
-                   }
+        socket.emit('score', { userId: randomId, score: $(event.target).data('score'), code: code }, (confirmation) => {
+            if (confirmation && confirmation.includes('Error')) {
+                   showConfirmMsg('房號無效，請重新加入', 0);
+                   localStorage.removeItem('sessionCode');
+                   setTimeout(() => {
+                        showLogin();
+                        $('.sessionCodeInput').val('');
+                        showConfirmMsg('');
+                   }, 1500);
+            } else {
+                   showConfirmMsg(confirmation, 0);
                    isScored = true;
-                }
-            });
-            
-        }
+            }
+        });
     });
 });
 
-socket.on('reset', () => {
-    dismissConfirmMsg();
-    localStorage.removeItem('userId');
-    isScored = false;
-    showStatusMessage('statusMsgConnected');
+socket.on('disconnect', () => {
+   $('.statusMsgConnected').hide(); 
+   $('.statusMsgDisconnected').show();
 });
 
 socket.on('connect', () => {
-    showStatusMessage('statusMsgConnected');
-    console.log(scoredUsers);
-    
+   $('.statusMsgDisconnected').hide();
+   $('.statusMsgConnected').show();
 });
 
-socket.on('disconnect', () => {
-    showStatusMessage('statusMsgDisconnected');
+socket.on('pushScore', (message) => {
+    console.log(message);
 });
 
-socket.on('authUser', (data) => {
-    console.log('authUser with user array from server:' + data.scoredUsers);
-    if (isScored) {
-        
-    } else if (checkScoredUser(data.scoredUsers)) {
-        showConfirmMsg('Already scored in this session.', 0);
-        isScored = true;
-    } else {
-        dismissConfirmMsg(0);
-    }
+socket.on('reset', (message) => {
+    // On system reset, maybe we should kick users out?
+    // Or just clear message.
+    showConfirmMsg(''); 
+    // Option: Force re-login?
+    // localStorage.removeItem('sessionCode');
+    // showLogin();
+    // But user asked for reset in admin page, usually to clear scores.
+    // If we change code, we should probably force logout.
+    // index.js regenerates code on reset.
+    // So the client's current code is now INVALID.
+    // So logic:
+    localStorage.removeItem('sessionCode');
+    showLogin();
+    showConfirmMsg('系統重置 (Reset)', 0);
 });
-
-
-function showConfirmMsg (message, speed = 200){
-    $('.confirmWrapper').animate({ top: 0, opacity: 1 }, speed).children('.confirmMsg').html(message);
-}
-
-function dismissConfirmMsg (speed = 200) {
-    $('.confirmWrapper').animate({ top: '220px', opacity: 0 }, speed).children('.confirmMsg').html(' ');
-}
-
-function showStatusMessage (className) {
-    const div = $('.' + className);
-    div.show();
-    div.siblings().hide();
-}
-
-function authUser(userIds) {
-    scoredUsers = userIds;
-    console.log(scoredUsers);
-    let localUserId = localStorage.getItem('userId');
-    if (localUserId && scoredUsers.includes(localUserId)) {
-        showConfirmMsg('Already scored in this session.', 0);
-        console.log('localUserId: ' + localUserId);
-    } else {
-        dismissConfirmMsg(0);
-    }
-}
-
-function checkScoredUser(userIdsFromServer) {
-    scoredUsers = userIdsFromServer;
-    console.log(scoredUsers);
-    let localUserId = localStorage.getItem('userId');
-    if (localUserId && scoredUsers.includes(localUserId)) {
-        console.log('localUserId: ' + localUserId);
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function rand () {
-    return Math.random().toString(36).substr(2); // remove `0.`
-};
